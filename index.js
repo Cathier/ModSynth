2c55d4a653eeeae22c5a400abf0c277e7b1c4dfe
Cathier <farciarz777@gmail.com>
2017-05-28 11:57:33 +0200

    28 May 11:57

--- index.js
+++ index.js
@@ -2,16 +2,83 @@
 /**
  * @name ModSynth
  */
+ 
+var time;
 
-class Oscillator
-{
-  constructor()
-  {
-    
+function sineWave(t, f) {
+  return Math.sin(Math.PI*2*t*f);
+}
+
+function squareWave(t, f) {
+  if((t*f)%1<0.5)
+    return -1;
+  else
+    return 1;
+}
+
+function sawWave(t, f) {
+  return 2*((t*f)%1)-1;
+}
+
+class Output {
+  constructor() {
+    this.out;
+  }
+}
+
+class Oscillator {
+  constructor(baseF, shiftF, gainF) {
+    this.baseF=baseF;
+    this.shiftF=shiftF;
+    this.gainF=gainF;
+    this.sineOut=new Output();
+    this.squareOut=new Output();
+    this.sawOut=new Output();
+  }
+  
+  update() {
+    var freq=baseF;
+    if(shiftF)
+    {
+      freq+=shiftF.out*gainF;
+    }
+    sineOut.out=sineWave(time, freq);
+    squareOut.out=squareWave(time, freq);
+    sawOut.out=sawWave(time, freq);
+  }
+  
+}
+
+class Mixer {
+  
+}
+
+class Speaker {
+  constructor(In) {
+    this.In=In;
+  }
+}
+
+class Modules {
+  constructor() {
+    this.modList=[]
+  }
+  
+  updateAll() {
+    modList.forEach(function(a) {
+      a.update();
+    });
+  }
+  
+  addModule(mod) {
+    modList.push(mod);
   }
 }
 
 export function dsp(t) {
+  time=t;
+  
+  
   return 0;
 }
 
